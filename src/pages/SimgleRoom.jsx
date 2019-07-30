43b0ea56a6f3cda03614d6ee15e25b7ad3e4a755
Dimetriu <dima.rashidov@gmail.com>
2019-07-30 10:50:10 +0300

    add Error page

--- src/pages/SimgleRoom.jsx
+++ src/pages/SimgleRoom.jsx
@@ -3,9 +3,7 @@ import React, { Component } from 'react';
 class SimgleRoom extends Component {
   render() {
     return (
-      <p>
-        Hello from Single Room page
-      </p>
+      <p>Hello from Single Room page</p>
     );
   }
 }
